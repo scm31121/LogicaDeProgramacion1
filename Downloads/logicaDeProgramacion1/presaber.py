113aaae5993b28a38519d7f8169a3bacf07c15ae
salas <salas@salas.local>
2026-02-09 07:09:52 -0500

    add comments and refactor names of variables

--- Downloads/logicaDeProgramacion1/presaber.py
+++ Downloads/logicaDeProgramacion1/presaber.py
@@ -1,8 +1,11 @@
+#Se piden datos al usurio
 base = float(input("ingrese la base del cuadrado: "))
 altura= float(input("ingrese la altura del cuadrado: "))
 
-def calcularArea(b,h):
-    a = (b * h)/2
-    return a
+#funcion para calcular el area
+def calcularArea(base,altura):
+    altura = (base * altura)/2
+    return altura
 
+#se retorna al usuario el resultado despues de procesarse
 print("el área del cuadrado es: ", calcularArea(base,altura))
